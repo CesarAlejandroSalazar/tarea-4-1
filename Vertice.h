64de50d8ebb54ccdb0bd643b421a8c8d1d07b361
CesarAlejandroSalazar <a01197841@itesm.mx>
2021-11-08 13:14:52 -0600

    Apuntadores Aristas
    Combinación de objetos arista y vertices

--- Vertice.h
+++ Vertice.h
@@ -1,5 +1,5 @@
 // Un archivo Vertice.cpp que contiene a la clase Vertice con los atributos privados id y dato y con los métodos get y set correspondientes a estos dos atributos //
-
+#include "Arista.h"
 #include <iostream>
 using namespace std;
 class Vertice{
@@ -9,7 +9,7 @@ class Vertice{
       dato = 0;
     }
   	
-  Vertice(string id, int dato){
+    Vertice(string id, int dato){
       this->id = id;
       this->dato = dato;
     }
@@ -29,9 +29,34 @@ class Vertice{
     int getDato(){
       return dato;
     }
+
+    void setNext(Arista *next){
+      this->next = next;
+    }
+    void setPrev(Arista *prev){
+      this->prev = prev;
+    }
+    Arista *getNext(){
+      return next;
+    }
+    Arista *getPrev(){
+      return prev;
+    }
+    void print()
+    {
+        Vertice *curr = head;
+        cout << "El contenido de la Lista" << endl;
+        while (curr != nullptr)
+        {
+            cout << curr->getDato() << endl;
+            curr = curr->getNext()->getNext();
+        }
+    }
     
   
   private:
     string id;
     int dato;
+    Vertice *head;
+    Arista *next, *prev;
 };
