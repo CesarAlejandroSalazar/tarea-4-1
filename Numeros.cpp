3c2272c4417d3ebe25f13b974eec5d7d2a9f91e6
Izel Avila <74568854+Iz-Idk@users.noreply.github.com>
2021-11-11 18:23:46 -0600

    Cambio de complejidad y documentacion

--- Numeros.cpp
+++ Numeros.cpp
@@ -7,33 +7,34 @@
 
 using namespace std;
 
-int main(){
-	vector<string> NumGuar{};
-	string DatoInsertado;
-  
-	cout<<"Escriba los numeros separados por comas"<<endl;
-	cin >> DatoInsertado;
-  char delimiter = ',';
+int main() {
+  // Declaración
+  char separador = ',';
   int count = 0;
+  vector < string > NumGuar {};
+  string DatoInsertado, Num;
+  Arista * listaArista[count];
 
+  cout << "Escriba los numeros separados por comas" << endl;
+  cin >> DatoInsertado;
+
+  // Leer el string insertado y poder modificarlo
   stringstream sstream(DatoInsertado);
-  string Num;
-  Arista *listaArista[count];
-  
-   while (getline(sstream, Num, delimiter)){
-     NumGuar.push_back(Num);
-     count ++;
-   }
 
-   for (int i = 0; i+1 < count; i++) {
-     Vertice *primero = new Vertice(1,NumGuar[i]);
-     Vertice *segundo = new Vertice(1,NumGuar[i+1]);
-     listaArista[i] = new Arista(primero,segundo);
-   }
+  // Lee el string DatoInsertado para identificar el elemento que los va a separar que en este caso será la coma. Va ciclando a lo largo del string y cortando hasta que se acabe. Con el push back guarda el dato de principio a fin.
+  while (getline(sstream, Num, separador)) {
+    NumGuar.push_back(Num);
+    count++;
+  }
 
-   for(int i = 0; i < count - 1;i++){
-     cout<<"En la arista " <<i+1<<" el primer vertice es: "<<listaArista[i]->getVi()->getDato()<<" y el segundo es: "<<listaArista[i]->getVj()->getDato()<<endl;
-   }
+  // Crear e insertar los nuevos vertices en las aristas
+  for (int i = 0; i + 1 < count; i++) {
+    Vertice * primero = new Vertice(1, NumGuar[i]);
+    Vertice * segundo = new Vertice(1, NumGuar[i + 1]);
+    listaArista[i] = new Arista(primero, segundo);
 
-    return 0;
+    cout << "En la arista " << i + 1 << " el primer vertice es: " << listaArista[i] -> getVi() -> getDato() << " y el segundo es: " << listaArista[i] -> getVj() -> getDato() << endl;
+  }
+
+  return 0;
 }
