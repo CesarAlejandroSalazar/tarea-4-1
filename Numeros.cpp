15a902d59ab50d5205ee5fca332eecc0d943fdb3
Izel Avila <74568854+Iz-Idk@users.noreply.github.com>
2021-11-11 18:27:02 -0600

    comentario

--- Numeros.cpp
+++ Numeros.cpp
@@ -22,12 +22,14 @@ int main() {
   stringstream sstream(DatoInsertado);
 
   // Lee el string DatoInsertado para identificar el elemento que los va a separar que en este caso será la coma. Va ciclando a lo largo del string y cortando hasta que se acabe. Con el push back guarda el dato de principio a fin.
+  ////////// Complejidad -> O(n) /////////
   while (getline(sstream, Num, separador)) {
     NumGuar.push_back(Num);
     count++;
   }
 
   // Crear e insertar los nuevos vertices en las aristas
+  //////// Complejidad -> O(n) ////////
   for (int i = 0; i + 1 < count; i++) {
     Vertice * primero = new Vertice(1, NumGuar[i]);
     Vertice * segundo = new Vertice(1, NumGuar[i + 1]);
@@ -35,6 +37,8 @@ int main() {
 
     cout << "En la arista " << i + 1 << " el primer vertice es: " << listaArista[i] -> getVi() -> getDato() << " y el segundo es: " << listaArista[i] -> getVj() -> getDato() << endl;
   }
+  
+  /////Complejidad total -> O(n^2)/////
 
   return 0;
 }
